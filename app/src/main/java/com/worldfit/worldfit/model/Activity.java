08041f8b5632301d220d2b53ba918dfd8ef6fbfa
adoankim <dkc.adam@gmail.com>
2015-01-31 17:28:00 +0100

    Changed activity type

--- app/src/main/java/com/worldfit/worldfit/model/Activity.java
+++ app/src/main/java/com/worldfit/worldfit/model/Activity.java
@@ -4,22 +4,22 @@ public class Activity {
 
     public final static String TYPE_STEPS = "steps";
 
-    private String type;
+    private String activity_type;
     private String date;
     private int value;
 
     public Activity(String type, String date, int value) {
-        this.type = type;
+        this.activity_type = type;
         this.date = date;
         this.value = value;
     }
 
-    public String getType() {
-        return type;
+    public String getActivity_type() {
+        return activity_type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setActivity_type(String activity_type) {
+        this.activity_type = activity_type;
     }
 
     public String getDate() {
