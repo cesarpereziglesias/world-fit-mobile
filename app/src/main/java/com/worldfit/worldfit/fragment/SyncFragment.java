7c23c88400cea1adc62ee4e3145fba6509f19694
adoankim <dkc.adam@gmail.com>
2015-01-31 13:38:20 +0100

    Refactoring SyncFragment

--- app/src/main/java/com/worldfit/worldfit/fragment/SyncFragment.java
+++ app/src/main/java/com/worldfit/worldfit/fragment/SyncFragment.java
@@ -16,17 +16,13 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
 import com.worldfit.worldfit.R;
+import com.worldfit.worldfit.util.FitApiWrapper;
 
 
 public class SyncFragment extends Fragment {
 
     private static final String TAG = "SyncFragment";
-    private static final int REQUEST_OAUTH = 1001;
-
-
-    private GoogleApiClient googleApiClient;
     private Activity parentActivity;
-    private boolean authInProgress = false;
 
     public static SyncFragment newInstance(String param1, String param2) {
         SyncFragment fragment = new SyncFragment();
@@ -42,74 +38,9 @@ public class SyncFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        buildFitnessClient();
-        googleApiClient.connect();
     }
 
-    /**
-     *  Build a {@link GoogleApiClient} that will authenticate the user and allow the application
-     *  to connect to Fitness APIs. The scopes included should match the scopes your app needs
-     *  (see documentation for details). Authentication will occasionally fail intentionally,
-     *  and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
-     *  can address. Examples of this include the user never having signed in before, or
-     *  having multiple accounts on the device and needing to specify which account to use, etc.
-     */
-    private void buildFitnessClient() {
-        // Create the Google API Client
-        googleApiClient = new GoogleApiClient.Builder(parentActivity.getApplicationContext())
-                .addApi(Fitness.API)
-                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
-                .addConnectionCallbacks(
-                        new GoogleApiClient.ConnectionCallbacks() {
-                            @Override
-                            public void onConnected(Bundle bundle) {
-                                Log.i(TAG, "Connected!!!");
 
-                            }
-
-                            @Override
-                            public void onConnectionSuspended(int i) {
-                                // If your connection to the sensor gets lost at some point,
-                                // you'll be able to determine the reason and react to it here.
-                                if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
-                                    Log.i(TAG, "Connection lost.  Cause: Network Lost.");
-                                } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
-                                    Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
-                                }
-                            }
-                        }
-                )
-                .addOnConnectionFailedListener(
-                        new GoogleApiClient.OnConnectionFailedListener() {
-                            // Called whenever the API client fails to connect.
-                            @Override
-                            public void onConnectionFailed(ConnectionResult result) {
-                                Log.i(TAG, "Connection failed. Cause: " + result.toString());
-                                if (!result.hasResolution()) {
-                                    // Show the localized error dialog
-                                    GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
-                                            parentActivity, 0).show();
-                                    return;
-                                }
-                                // The failure has a resolution. Resolve it.
-                                // Called typically when the app is not yet authorized, and an
-                                // authorization dialog is displayed to the user.
-                                if (!authInProgress) {
-                                    try {
-                                        Log.i(TAG, "Attempting to resolve failed connection");
-                                        authInProgress = true;
-                                        result.startResolutionForResult(parentActivity,
-                                                REQUEST_OAUTH);
-                                    } catch (IntentSender.SendIntentException e) {
-                                        Log.e(TAG,
-                                                "Exception while starting resolution activity", e);
-                                    }
-                                }
-                            }
-                        }
-                )
-                .build();
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
